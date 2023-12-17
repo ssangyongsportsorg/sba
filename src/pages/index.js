7c757589e144165b5c0500abeaaf67be89a0dfe8
BRIAN <107471917+sxadxsx@users.noreply.github.com>
2023-12-17 22:19:53 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,233 +33,64 @@ export default function Home() {
       description="​雙龍職籃 ... 籃球，為一種由兩隊參與，在一個長方形籃球場進行的球類運動。每隊出場5名隊員，可將球向任何方向傳、投、拍、滾或運，目的是將籃球投入對方球籃得分，並阻止 ...">
       <main>
       <>
-  <div className="header">
-    <div className="container">
-      <div className="hero-content">
-        <div className="hero-block">
-          <div className="content-block-hero">
-            <h1 className="heading-2">
-              <strong className="bold-text-9">我們的</strong>
-              <span>
-                <strong className="bold-text">使命</strong>
-              </span>
-              <strong className="bold-text-9">是將</strong>
-              <span>
-                <strong className="bold-text-3">雙</strong>
-              </span>
-              <strong className="bold-text-3">龍</strong>
-              <span>
-                <strong className="bold-text-3">體</strong>
-                <strong className="bold-text-3">育</strong>
-              </span>
-              <strong className="bold-text-10">組成</strong>
-              <span>
-                <strong className="bold-text-6">最棒</strong>
-             </span>
-              <strong className="bold-text-10">體育組織</strong>
-              <span>
-              </span>
-              <span className="cursor">
-                <strong className="cursor" data-owner="main"></strong>
-              </span>
+   <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
+        </div>
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              Announcing our next round of funding.{' '}
+              <a href="#" className="font-semibold text-indigo-600">
+                <span className="absolute inset-0" aria-hidden="true" />
+                Read more <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Data to enrich your online business
             </h1>
-            <div className="button-wrap-hero">
-            <a
-                className="_6 w-inline-block"
-                data-w-id="d90f4fb2-2134-be56-aa63-3fe5d940d2aa"
-                href="#123"
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+              fugiat veniam occaecat fugiat aliqua.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                        <ReactiveButton shadow size="large" idleText="我們的體育" />
-
+                Get started
+              </a>
+              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                Learn more <span aria-hidden="true">→</span>
               </a>
             </div>
           </div>
         </div>
-        <div className="hero-image-wrap">
-          <img
-            alt=""
-            className="hero-image"
-            src="https://74-rho.vercel.app/images/undraw_fitness_stats_sht6.svg"
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
           />
         </div>
       </div>
     </div>
-  </div>
-  <div className="section-content">
-    <div className="container space-below" id={123}>
-      <div className="title-wrap-left">
-        <p className="title">OUR&nbsp;sport</p>
-        <h3 className="heading-primary">
-          雙龍體育
-          <span className="text-span">的運動</span>
-        </h3>
-      </div>
-      <div className="service-card-wrap">
-        <div className="column">
-          <div className="service-card">
-            <img
-              alt=""
-              className="service-icon"
-              src="https://74-rho.vercel.app/images/baseball-16.png"
-              width={75}
-            />
-            <div className="card-content-wrap">
-              <h3 className="heading-3">雙龍職棒</h3>
-              <p className="paragraph-2">
-                <strong>
-                  棒球，（英語：Baseball、日韓稱為野球），為一種團體球類運動，由人數最少為9人的两支队伍在一個扇形的球場进行攻擊與守備。棒球球員分為攻...
-                </strong>
-              </p>
-            </div>
-          
-            <a href="https://www.slbtw.cf/" target="_blank">
-    <ReactiveButton color="yellow" idleText="更多消息" />
-  </a>
-          </div>
-        </div>
-        <div className="column">
-          <div className="service-card">
-            <img
-              alt=""
-              className="service-icon"
-              src="https://74-rho.vercel.app/images/889455.png"
-              width={75}
-            />
-            <div className="card-content-wrap">
-              <h3 className="heading-3">​雙龍職籃</h3>
-              <p className="paragraph-2">
-                <strong>
-                  籃球，為一種由兩隊參與，在一個長方形籃球場進行的球類運動。每隊出場5名隊員，可將球向任何方向傳、投、拍、滾或運，目的是將籃球投入對方球籃得分，並阻止對方獲得控球權或得...
-                </strong>
-              </p>
-            </div>
-            <a href="https://www.sbatw.ml" target="_blank">
-    <ReactiveButton idleText="更多消息" />
-  </a>
-          </div>
-        </div>
-        <div className="column">
-          <div className="service-card">
-            <img
-              alt=""
-              className="service-icon"
-              src="https://74-rho.vercel.app/images/53283.png"
-              width={75}
-            />
-            <div className="card-content-wrap">
-              <h3 className="heading-3">雙龍足球</h3>
-              <p className="paragraph-2">
-                <strong>
-                  足球（football）主要專指英式足球，官方名為協會足球（association
-                  football，簡稱soccer），是一種世界流行的團體球類運動，也是所有體育運動中最受歡迎、接受度最高、普及面最廣的一種，被譽為「世界遊戲」
-                </strong>
-              </p>
-            </div>
-            <a href="/" target="_blank">
-    <ReactiveButton color="teal" idleText="更多消息" />
-  </a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="container">
-      <div className="column-wrap-reversed">
-        <div className="image-block">
-          <img
-            alt=""
-            data-w-id="cd4d931f-771b-3662-2f2f-9a8f90ae76b5"
-            src="https://74-rho.vercel.app/images/62d564600ff95899fb6eda9d_75df1528-2822-4edf-9c6a-8cd50c0c2c28-p-500.png"
-          />
-        </div>
-        <div className="content-block-left">
-          <div className="content-wrap">
-            <p className="title">
-              slb
-              <br />
-            </p>
-            <h2 className="heading-primary">​雙龍職棒</h2>
-            <p className="paragraph">
-              <strong>
-                棒球，（英語：Baseball、日韓稱為野球），為一種團體球類運動，由人數最少為9人的两支队伍在一個扇形的球場进行攻擊與守備。棒球球員分為攻...
-              </strong>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="column-wrap">
-        <div className="image-block">
-          <img
-            alt=""
-            src="https://74-rho.vercel.app/images/62d56332e87b47c78b67ac6c_taxi-basketball.png"
-            width={398}
-          />
-              <p>&nbsp;</p>
-  <p>
-    <br />
-  </p>
-        </div>
-  
-        <div className="content-block-right">
-          <div className="content-wrap">
-            <p className="title">sba</p>
-            <h2 className="heading-primary">​雙龍職籃</h2>
-            <p className="paragraph">
-              <strong>
-                籃球，為一種由兩隊參與，在一個長方形籃球場進行的球類運動。每隊出場5名隊員，可將球向任何方向傳、投、拍、滾或運，目的是將籃球投入對方球籃得分，並阻止對方獲得控球權或得分。籃球是世界上最多人觀看
-              </strong>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div className="section-statistics">
-    <div className="container">
-      <div className="title-wrap">
-        <p className="title">about ssport</p>
-        <h3 className="heading-primary h3-centre">
-          關於我們
-          <br />
-        </h3>
-      </div>
-      <div className="feature-number-wrap">
-        <div className="statistic-circle-wrap">
-          <div className="circle-div">
-            <h4 className="counterup">2017</h4>
-            <div className="title-capped">創建雙龍體育</div>
-          </div>
-        </div>
-        <div className="arrow-wrap">
-          <img alt="" className="arrow-image" src="https://site123-git-main-ssangyongsports.vercel.app/images/arrow-right.svg" />
-          <img
-            alt=""
-            className="arrow-image-mobile"
-            src="https://site123-git-main-ssangyongsports.vercel.app/images/arrow-down.svg"
-          />
-        </div>
-        <div className="statistic-circle-wrap">
-          <div className="circle-div">
-            <h1 className="counterup">2019</h1>
-            <div className="title-capped">創建sba,slb</div>
-          </div>
-        </div>
-        <div className="arrow-wrap">
-          <img alt="" className="arrow-image" src="https://site123-git-main-ssangyongsports.vercel.app/images/arrow-right.svg" />
-          <img
-            alt=""
-            className="arrow-image-mobile-02"
-            src="https://site123-git-main-ssangyongsports.vercel.app/images/arrow-down-02.svg"
-          />
-        </div>
-        <div className="statistic-circle-wrap">
-          <div className="circle-div">
-            <h1 className="counterup">2022</h1>
-            <div className="title-capped">收購js</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 </>
       </main>
     </Layout>
